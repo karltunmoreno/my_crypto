e10c628259312f83e271b945e01ac2ab0c03d78d
Julian Esquivel <julianesquivel@Julians-MacBook-Pro-2.local>
2022-04-06 17:41:05 -0400

    handlebars connected

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,22 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Coins, User } = require('../models');
+const router = require("express").Router();
 
+// coin JSON data
+const { coinsScrap } = require("../models/coinsScrap");
+
+router.get("/", (req, res) => {
+  res.render("main", { layout: "index", coins: coinsScrap });
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { layout: "index" });
+});
+
+router.get("/myCrypto", (req, res) => {
+  res.render("myCrypto", { layout: "index", coins: coinsScrap });
+});
+
+router.get("/coinConverter", (req, res) => {
+  res.render("coinConverter", { layout: "index" });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,8 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes');
+const router = require("express").Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
-
-
-//router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
