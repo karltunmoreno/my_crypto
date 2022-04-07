4291e87e109ec66539f6e1f21d195c2315422af5
Julian Esquivel <julianesquivel@Julians-MacBook-Pro-2.local>
2022-04-07 16:45:12 -0400

    coin converter and login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,15 @@
 const router = require("express").Router();
 
+const { User } = require("../models");
 // coin JSON data
 const { coinsScrap } = require("../models/coinsScrap");
 
 router.get("/", (req, res) => {
-  res.render("main", { layout: "index", coins: coinsScrap });
+  res.render("main", { layout: "index", coins: coinsScrap, user: User });
 });
 
 router.get("/login", (req, res) => {
-  res.render("login", { layout: "index" });
+  res.render("login", { layout: "index", user: User });
 });
 
 router.get("/myCrypto", (req, res) => {
