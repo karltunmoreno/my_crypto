b83d19a33300a599ef20549b4d89907b1a264bdd
Abraham Aguirre <aguirre.abraham@gmail.com>
2022-04-08 00:13:16 -0400

    fixed single quotes

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,21 +1,21 @@
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    const response = await fetch("/api/users/login", {
-      method: "post",
+    const response = await fetch('/api/users/login', {
+      method: 'post',
       body: JSON.stringify({
         email,
         password,
       }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
@@ -25,23 +25,23 @@ async function loginFormHandler(event) {
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
   if (username && email && password) {
-    const response = await fetch("/api/users", {
-      method: "post",
+    const response = await fetch('/api/users', {
+      method: 'post',
       body: JSON.stringify({
         username,
         email,
         password,
       }),
-      headers: { "Content-Type": "application/json" },
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
@@ -49,9 +49,9 @@ async function signupFormHandler(event) {
 }
 
 document
-  .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
 
 document
-  .querySelector(".signup-form")
-  .addEventListener("submit", signupFormHandler);
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
