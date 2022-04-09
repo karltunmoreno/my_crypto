a01185269b1771f181d82d34a24a70fd4ca2c824
Abraham Aguirre <aguirre.abraham@gmail.com>
2022-04-09 14:09:07 -0400

    commas are dumb

--- controllers/api/crypto_routes.js
+++ controllers/api/crypto_routes.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
     try {
       const config = {
         method: 'get',
-          url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?sort=cmc_rank&limit=100https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?cryptocurrency_type=coins&aux=cmc_rank&limit=100',
+        url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?cryptocurrency_type=coins&aux=cmc_rank&limit=100',
         headers: { 
           'X-CMC_PRO_API_KEY': 'd3bcf3f1-342e-490a-86e2-484d000fc31b'
         }
